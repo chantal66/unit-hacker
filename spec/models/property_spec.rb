79c705affd1dd596161e1f209a5ff239352c0c2b
Chantal Justamond <cjustamm66@me.com>
2018-04-02 14:54:02 -0600

    properties can be created with a name

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Property, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation' do
+    it 'is invalid without a name' do
+      property = Property.new()
+      property2 = Property.create(name: nil)
+
+      expect(property).to be_invalid
+      expect(property2).to be_invalid
+    end
+
+    it 'is valid with a name' do
+      property = Property.create!(name: 'Greenwood')
+
+      expect(property).to be_valid
+    end
+  end
 end
