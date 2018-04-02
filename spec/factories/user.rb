315f1056f8cae4099009bb5bcbb54d7206861015
Chantal Justamond <cjustamm66@me.com>
2018-04-01 18:52:03 -0600

    - factory added for admin user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -10,4 +10,12 @@ FactoryBot.define do
     password 'asdfasdf'
     password_confirmation 'asdfasdf'
   end
+
+  factory :admin_user, class: 'AdminUser' do
+    first_name 'Admin'
+    last_name 'User'
+    email { generate :email }
+    password 'asdfasdf'
+    password_confirmation 'asdfasdf'
+  end
 end
