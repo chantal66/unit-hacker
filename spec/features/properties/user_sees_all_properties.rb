c57ce2d5a02f621141ba5f3aea7c4eb984a21c17
Chantal Justamond <cjustamm66@me.com>
2018-04-02 13:57:41 -0600

    test for properties_path :ok_hand:

--- spec/features/properties/user_sees_all_properties.rb
+++ spec/features/properties/user_sees_all_properties.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'index' do
+    before  do
+      @property = FactoryBot.create(:property)
+    end
+
     it 'can be reached successfully' do
-      property = FactoryBot.create(:property)
       visit properties_path
 
       expect(page.status_code).to eq(200)
+      expect(page).to have_content(/Properties/)
     end
-
   end
 end
