16e2902f5659619ee500a33cf8acd4274f52dd3c
Chantal Justamond <cjustamm66@me.com>
2018-04-10 11:40:11 -0600

    user can edit a property :smile_cat:

--- spec/features/properties/user_can_edit_an_existing_property_spec.rb
+++ spec/features/properties/user_can_edit_an_existing_property_spec.rb
@@ -5,17 +5,16 @@ describe 'User can edit an existing property' do
     before(:each) do
       @user = FactoryBot.create(:user)
       login_as(@user, :scope => :user)
-
-      @property = FactoryBot.create(:property)
     end
 
     it 'can edit a property' do
-      visit edit_property_path(@property)
+      property = Property.create!(name: 'Greenwood', user_id: @user.id)
+      visit edit_property_path(property)
 
       fill_in 'property[name]', with: 'Salamandra States'
       click_on 'Save'
 
-      expect(current_path).to eq("/properties/#{property.last.id}/apartments")
+      expect(current_path).to eq("/properties/#{property.id}/apartments")
       expect(page).to have_content('Salamandra States')
       expect(page).to_not have_content('Greenwood')
     end
