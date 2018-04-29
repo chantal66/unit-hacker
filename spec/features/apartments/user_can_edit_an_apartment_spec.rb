b2b9c0d4c80c450b978d6b381f3b81f1a24060ec
Chantal Justamond <cjustamm66@me.com>
2018-04-29 17:13:24 -0600

    test added for editing an apartment

--- spec/features/apartments/user_can_edit_an_apartment_spec.rb
+++ spec/features/apartments/user_can_edit_an_apartment_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
-describe 'Edit' do
-  it '' do
-    
+describe  'Edit' do
+  it  'user can create a edit an apartment' do
+    user = FactoryBot.create(:user)
+    login_as(user, :scope => :user)
+    property = FactoryBot.create(:property)
+    FactoryBot.create(:apartment)
+
+
+    visit edit_property_apartment_path(property)
+
+    fill_in 'apartment[unit_number]', with: '102'
+    fill_in 'apartment[beds]', with: '2'
+    fill_in 'apartment[baths]', with: '2'
+    select 'Ground', from: 'apartment[unit_location]'
+    select 'false', from: 'apartment[garage]'
+    select  'Partial', from:  'apartment[renovation_type]'
+    click_button 'Save'
+
+    expect(current_path).to eq("/properties/#{property.id}/apartments/#{Apartment.last.id}")
+    expect(page).to have_content('102')
+    expect(page).to have_content('2')
+    expect(page).to have_content('2')
+    expect(page).to have_content('Ground')
+    expect(page).to have_content('false')
+    expect(page).to have_content('Partial')
   end
 end
+
